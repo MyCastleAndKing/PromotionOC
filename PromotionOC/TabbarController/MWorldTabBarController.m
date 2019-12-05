398980116043aa0e9a35524b63479092dd3fdc83
“我的国王我的城” <meiyang_1228@163.com>
2019-12-05 18:41:12 +0800

    设置tabBar状态

--- PromotionOC/TabbarController/MWorldTabBarController.m
+++ PromotionOC/TabbarController/MWorldTabBarController.m
@@ -8,7 +8,7 @@
 
 #import "MWorldTabBarController.h"
 
-@interface MWorldTabBarController ()
+@interface MWorldTabBarController () <UITabBarControllerDelegate>
 
 @end
 
@@ -18,6 +18,77 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+
+/// 构建tabBar数组
+- (NSArray *)viewControllerForTabBar {
+    // first
+    FirstPartyTableViewController *firstVC = [[FirstPartyTableViewController alloc] init];
+    UIViewController *firstNav = [[CYLBaseNavigationController alloc] initWithRootViewController:firstVC];
+    [firstNav cyl_setHideNavigationBarSeparator:YES];
+    // second
+    SecondPartyViewController *secondVC = [[SecondPartyViewController alloc] init];
+    UIViewController *secondNav = [[CYLBaseNavigationController alloc] initWithRootViewController:secondVC];
+    [secondNav cyl_setHideNavigationBarSeparator:YES];
+    NSArray *viewControllers = @[firstNav, secondNav];
+    
+    return viewControllers;
+}
+
+
+/// 设置tabBar属性
+- (NSArray *)buildAttributeStatusForTabBarItems {
+    CGFloat firstXOffset = -12/2;
+    // first
+    NSDictionary *firstTabBarItemsAttr = @{CYLTabBarItemTitle:@"首页",
+                                           CYLTabBarItemImage:[UIImage imageNamed:@"home_normal"],
+                                           CYLTabBarItemSelectedImage:[UIImage imageNamed:@"home_highlight"],
+                                           CYLTabBarItemTitlePositionAdjustment:[NSValue valueWithUIOffset:UIOffsetMake(firstXOffset, -3.5)],
+                                           CYLTabBarLottieURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"tab_home_animate" ofType:@"json"]],
+    };
+    // second
+    CGFloat secondXOffset = (-25+2)/2;
+    NSDictionary *secondTabBarItemAttr = @{CYLTabBarItemTitle:@"鱼塘",
+                                           CYLTabBarItemImage:[UIImage imageNamed:@"fishpond_normal"],
+                                           CYLTabBarItemSelectedImage:[UIImage imageNamed:@"fishpond_highlight"],
+                                           CYLTabBarItemTitlePositionAdjustment:[NSValue valueWithUIOffset:UIOffsetMake(secondXOffset, -3.5)],
+                                           CYLTabBarLottieURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"tab_search_animate" ofType:@"json"]],
+    };
+    NSArray *tabBarItemAttributes = @[firstTabBarItemsAttr, secondTabBarItemAttr];
+    
+    return tabBarItemAttributes;
+}
+
+
+/// 设置更多自定义tabBarItem属性 选中文字|不选中文字 背景图片 等等
+- (void)customTabBarAppearance {
+    // 自定义tabBar高度
+    // self.tabBarHeight = CYL_IS_IPHONE_X ? 65 : 40;
+    
+    [self rootWindow].backgroundColor = [UIColor cyl_systemBackgroundColor];
+    // 普通状态下的文字属性
+    NSMutableDictionary *normalAttrs = [[NSMutableDictionary alloc] init];
+    [normalAttrs setValue:[UIColor cyl_systemGrayColor] forKey:NSForegroundColorAttributeName];
+    // 选中状态下的文字属性
+    NSMutableDictionary *selectAttrs = [[NSMutableDictionary alloc] init];
+    [selectAttrs setValue:[UIColor cyl_labelColor] forKey:NSForegroundColorAttributeName];
+    // 设置文字属性
+    UITabBarItem *tabBarItem = [UITabBarItem appearance];
+    [tabBarItem setTitleTextAttributes:normalAttrs forState:UIControlStateNormal];
+    [tabBarItem setTitleTextAttributes:selectAttrs forState:UIControlStateSelected];
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
 
 /*
 #pragma mark - Navigation
